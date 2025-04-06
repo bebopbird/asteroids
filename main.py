5dec6e3db4438a44a14a4ba6282d1d7f44a45fd6
bird_gs <gs.goncalosoares@gmail.com>
2025-04-07 00:07:07 +0100

    C:Shooting Asteroids

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -17,9 +20,18 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+
 
     dt = 0
 
@@ -32,9 +44,20 @@ def main():
 
         screen.fill("black")
 
+        for asteroid in asteroids:
+            if asteroid.collide_with(player):
+                print("Game over!")
+                sys.exit()
+
         for obj in drawable:
             obj.draw(screen)
         
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collide_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         pygame.display.flip()
 
         # limit to 60fps
